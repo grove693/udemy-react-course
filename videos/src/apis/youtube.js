dac4bfb300f3f6c23e5045c0a4afb573030ec83e
Anghel Aranyos <anghel.aranyos@worldpay.com>
2019-09-12 17:51:51 +0300

    Section 11: 128

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const KEY = 'AIzaSyDgVTpsixpqTGB8M-G-CaYsQeuKeapeQa4';
+
+
+
+export default axios.create(
+    {
+        baseURL: 'https://www.googleapis.com/youtube/v3',
+        params:{
+            part: 'snippet',
+            maxResults:5,
+            key: KEY
+        }
+    }
+);
