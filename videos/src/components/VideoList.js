b2638d84eb1122813a5a369e5b0ce9cd311d527a
Anghel Aranyos <anghel.aranyos@worldpay.com>
2019-09-13 14:09:27 +0300

    Section 11: video app finished

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ( {videos})=> {
+const VideoList = ( {videos, onVideoSelect})=> {
     //props.videos
    const renderedList =  videos.map(
         (video) => {
-            return <VideoItem video = {video} />;
+            return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video = {video} />;
         });
     return(
-        <div>{renderedList}</div>
+        <div className="ui relaxed divided list">{renderedList}</div>
     );
 };
 
