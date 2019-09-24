59ce8f2c05b8a578fedf3720ccd8a2f06c67b260
Anghel Aranyos <anghel.aranyos@worldpay.com>
2019-09-24 08:50:04 +0300

    Section 15:Redux Store Design

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonplaceholder';
 
-export const fetchPostsAndUsers = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
 
   console.log('About to fetch posts');
   await dispatch(fetchPosts());
-  const userIds = _.uniq._map(getState().posts, 'userId');
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
 
   userIds.forEach(id => dispatch(fetchUser(id)));
 
@@ -16,7 +16,7 @@ export const fetchPostsAndUsers = () => async dispatch => {
 
 export const fetchPosts =  () => {
 
-    return async  (dispatch, getState)  =>  {
+    return async  (dispatch)  =>  {
         const response = await jsonPlaceholder.get('/posts');
 
         dispatch({type: 'FETCH_POSTS', payload: response.data })
